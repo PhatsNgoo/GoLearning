e5856d196ffc70d990e4d985d4bb86aabce1fa5a
DESKTOP-5DTDNTA\ASUS <phatsngoo2702@gmail.com>
2019-09-18 22:44:27 +0700

    Intitial project with gin gonic

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
-import stringUtils "./StringUtils"
+import "net/http"
+import "github.com/gin-gonic/gin"
+
+var router *gin.Engine
 
 func main()  {
-	fmt.Println(stringUtils.Reverse("olleh, dlroW"))
+	//Creates a default route
+	router :=gin.Default()
+
+	//Load templates directory
+	router.LoadHTMLGlob("templates/*")
+
+	//Define router handler
+	router.GET("/", func(c *gin.Context) {
+		//Call the html method
+		c.HTML(
+			//Set the HTTP status to 200 OK
+			http.StatusOK,
+			//Use Index html file
+			"index.html",
+			//Pass the data that the page uses(in this case 'title')
+			gin.H{
+				"title":"HomePage",
+			},
+			)
+	})
+	//Start serving the application
+	router.Run()
+
 }
