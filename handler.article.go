6958a5efbc87e5a699339a0c19c2203ba35435ce
DESKTOP-5DTDNTA\ASUS <phatsngoo2702@gmail.com>
2019-09-21 23:03:00 +0700

    Respond with json/xml format

--- handler.article.go
+++ handler.article.go
@@ -12,18 +12,23 @@ import (
 func showIndexPage(c *gin.Context) {
 	articles := GetAllArticle()
 
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		// Set the HTTP status to 200 (OK)
-		http.StatusOK,
-		// Use the index.html template
-		"index.html",
-		// Pass the data that the page uses
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	//// Call the HTML method of the Context to render a template
+	//c.HTML(
+	//	// Set the HTTP status to 200 (OK)
+	//	http.StatusOK,
+	//	// Use the index.html template
+	//	"index.html",
+	//	// Pass the data that the page uses
+	//	gin.H{
+	//		"title":   "Home Page",
+	//		"payload": articles,
+	//	},
+	//)
+	render(c,gin.H{
+		"title":"Home Page",
+		"payload": articles,
+	},
+	"index.html")
 
 }
 
@@ -51,4 +56,20 @@ func getArticle(c *gin.Context){
 		//If an invalid article ID is specified in the URL,abort with an error
 		c.AbortWithStatus(http.StatusNotFound)
 	}
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		// Respond with JSON
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		// Respond with XML
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		// Respond with HTML
+		c.HTML(http.StatusOK, templateName, data)
+	}
+
 }
